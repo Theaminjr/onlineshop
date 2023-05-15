b6f2b247b987efa9cb4bcb473b67e6ae3ce8c15b
Theaminjr <aminnajarisisi@gmail.com>
2023-05-15 10:43:35 +0330

    commented

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User,Sale
 
+"custom admin panel for the custom user"
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['email', 'full_name', 'phone_number', 'is_active', 'is_staff', 'is_superuser','is_productmanager','is_staff','is_operator']
