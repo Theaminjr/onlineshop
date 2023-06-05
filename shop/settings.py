fe54c8f3c7bf3483fac832f2aceb3598b799cb25
Amin Najari <121177817+Theaminjr@users.noreply.github.com>
2023-06-05 11:34:36 +0330

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -152,4 +152,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noucampmaktab@gmail.com'
-EMAIL_HOST_PASSWORD = 'zdzeuhwdsqdcpasu'
+EMAIL_HOST_PASSWORD = ''
